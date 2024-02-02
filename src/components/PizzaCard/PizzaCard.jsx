cdd40f1419a234c6efd0ca32e0709f6edd1e34b6
Ryan Barfield <cadium828@gmail.com>
2024-02-01 19:04:36 -0600

    updated pizza list and pizza card

--- src/components/PizzaCard/PizzaCard.jsx
+++ src/components/PizzaCard/PizzaCard.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
-function PizzaCard() {
-  return;
+function PizzaCard(pizza) {
+  return (
+    <div>
+      <div className="imgContainer">
+        <img src={pizza.image_path} />
+      </div>
+      <div className="pizza-title-container">
+        <h2>{pizza.name}</h2>
+        <p>{pizza.description}</p>
+      </div>
+      <div>
+        <h3>{pizza.price}</h3>
+      </div>
+      <button>Add Pizza</button>
+    </div>
+  );
 }
 
 export default PizzaCard;
